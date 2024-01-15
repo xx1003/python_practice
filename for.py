2a1cb8dd16e065355d1294c1711e93ac750cf5da
xx1003 <wntnwls2002@gmail.com>
2024-01-15 18:51:28 +0900

    3.1.2 function

--- for.py
+++ for.py
@@ -1,5 +1,10 @@
-num = int(input())
-i = 0
-for i in range(num):
-    i += 1
-    print(f'{i} {i*i}')
+def gugudan():
+    i = 2
+    while i != 10:
+        for n in range(1, 10):
+            print(f'{i} * {n} = {i*n}')
+
+        i += 1
+
+
+gugudan()
